66e88a8a71ac66dd285da25643a1c9be52e188b1
debaosuidecl@gmail.com <debaosuidecl@gmail.com>
2019-10-29 04:04:00 +0100

    fixed download

--- downloadCSV.js
+++ downloadCSV.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 9808;
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://167.99.124.182:8080'); // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
