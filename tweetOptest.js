c6c73f7205498d64925a8d45e8d42f47f51ea1b6
debaosuidecl@gmail.com <debaosuidecl@gmail.com>
2019-11-09 13:34:58 +0100

    D

--- tweetOptest.js
+++ tweetOptest.js
@@ -149,6 +149,11 @@ const startTweet = async (
     // return;
     await verifiedUser.save();
   } catch (error) {
+    let verifiedUserToChangeDoNotRepeat = await VerifiedUserData.findOne({
+      email: data.email
+    });
+    verifiedUserToChangeDoNotRepeat.doNotRepeat = true;
+    verifiedUserToChangeDoNotRepeat.save();
     console.log(error);
     console.log(
       'not changed active state ... or updated baseLink... account probably terminated'
@@ -322,6 +327,56 @@ const startTweet = async (
                   }
 
                   // try for already verified
+                  try {
+                    await page.waitForSelector(
+                      `form[action="/account/access"]`
+                    );
+                    const button = await page.waitForSelector(
+                      `input.Button[value="Start"]`
+                    );
+
+                    console.log(
+                      'the button from access asking us for verification again.... we gotta kill it!'
+                    );
+
+                    let verifiedUserToKill = await VerifiedUserData.findOne({
+                      email: data.email
+                    });
+                    verifiedUserToKill.failures = [
+                      ...verifiedUserToKill.failures,
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail',
+                      'fail'
+                    ];
+                    verifiedUserToKill.doNotRepeat = true;
+
+                    verifiedUserToKill.save();
+                    console.log(
+                      'the account was killed for asking for access again!!!'
+                    );
+                  } catch (e) {
+                    console.log('did not ask me to verify account again');
+                  }
 
                   try {
                     await page.waitForSelector(
@@ -354,9 +409,7 @@ const startTweet = async (
                   await page.waitForSelector(
                     `path[d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"]`
                   );
-                  // const randomFileName = `tweetLink.csv`;
 
-                  let date = new Date();
                   let randomFileName = `tweetNew${data.offer}.csv`;
                   await fs.ensureFile(randomFileName);
                   console.log('File ensured');
@@ -364,8 +417,7 @@ const startTweet = async (
                   let compose = await page.$(
                     `path[d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"]`
                   );
-                  // compose.click();
-                  // await page.click(0.5, 0.5);
+
                   await page.mouse.click(0.5, 0.5);
                   for (let j = 0; j < numberOfRuns; j++) {
                     if (numberOfRuns % 9 === 0) {
@@ -410,7 +462,7 @@ const startTweet = async (
                               email
                             }
                           );
-                          console.log(verifiedUserToRegisterFailure);
+                          // console.log(verifiedUserToRegisterFailure);
                           verifiedUserToRegisterFailure.failures.push('fail');
                           // return;
                           await verifiedUserToRegisterFailure.save();
